62cba77427670a70f22539223c91dda1afd67b01
Atom Ulikhanyan <atomulikhanyan@gmail.com>
2018-03-29 14:49:54 +0400

    Twitter login page email and password check

---
+++ conf.js
@@ -0,0 +1,11 @@
+'use strict';
+exports.config = {
+	framework: 'jasmine2',
+	seleniumAddress: 'http://localhost:4444/wd/hub',
+	specs : ['loginPagePositiveTesting.js'],
+	multiCapabilities: [{
+	    browserName: 'firefox'
+	  }, {
+	    browserName: 'chrome'
+	  }]
+};
---
+++ loginPagePositiveTesting.js
@@ -0,0 +1,99 @@
+
+'use strict';
+
+const poTW = require('./pageObj.json');
+const test_data = require('./loginPagePositiveTesting.json');
+
+describe('Twitter: Email validation ', ()=> {
+	const validEmail = $(poTW.loginField.validEmail);
+ 	const emailError = $(poTW.loginField.error);
+
+	beforeAll(()=> {
+		browser.ignoreSynchronization = true;
+		browser.get(poTW.loginField.signUpURL);
+		browser.manage().window().maximize();	
+	});
+
+	afterAll(() => {
+		browser.ignoreSynchronization = false;
+		browser.executeScript('window.sessionStorage.clear();');
+		browser.executeScript('window.localStorage.clear();');
+	});
+
+	const posCheck = ()=> {
+		const posVal = test_data.emailPosValue; 
+		for (var i = 0; i < posVal.length; ++i) {
+			$(poTW.loginField.singUpEmail).clear().sendKeys(posVal[i]);
+			browser.sleep(1000);
+			expect(validEmail.isDisplayed()).toBe(true);
+			expect(emailError.isPresent()).toBeFalsy();
+		}	
+	}
+
+	it('should check positive values ', ()=> {
+		posCheck();
+	});
+
+	const negCheck = ()=> {
+		const negVal = test_data.emailNegValues;
+		for (var i = 0; i < negVal.length; ++i) {
+			$(poTW.loginField.singUpEmail).clear().sendKeys(negVal[i]);
+			browser.sleep(1000);
+			expect(validEmail.isPresent()).toBeFalsy();
+			expect(emailError.getText()).toBe(test_data.emailErrorMessage);
+		}
+	}
+
+	it('should check negative values ', ()=> {
+		negCheck();
+	});
+
+});
+
+
+
+describe('Twitter: Password validation ', ()=> {
+
+	beforeAll(()=> {
+		browser.ignoreSynchronization = true;
+		browser.get(poTW.loginField.signUpURL);
+		browser.manage().window().maximize();	
+	});
+
+	afterAll(() => {
+		browser.ignoreSynchronization = false;
+		browser.executeScript('window.sessionStorage.clear();');
+		browser.executeScript('window.localStorage.clear();');
+	});
+
+	const passwordPosCheck = ()=> {
+		const posVal = test_data.passwordPosValues; 
+		for (var i = 0; i < posVal.length; ++i) {
+			$(poTW.loginField.passwordInput).clear().sendKeys(posVal[i]);
+			browser.sleep(5000);
+			expect( element(By.xpath('//*[@id="phx-signup-form"]/div[1]/div[3]/div[1]/div/p[1]')).isDisplayed()).toBe(true);
+			expect($(poTW.loginField.error).isPresent()).toBeFalsy();
+		}	
+	}
+
+	it('should check password positive values ', ()=> {
+		passwordPosCheck();
+	});
+
+	const passwordNegCheck = ()=> {
+		const negVal = test_data.passwordNegValues;
+		for (var i = 0; i < negVal.length; ++i) {
+			$(poTW.loginField.passwordInput).clear().sendKeys(negVal[i]);
+			browser.sleep(5000);
+			expect($(poTW.loginField.validPassword).isPresent()).toBeFalsy();
+			expect($(poTW.loginField.error).getText()).toBe(test_data.passordErrorMessage);
+		}
+	}
+
+	it('should check password negative values ', ()=> {
+		passwordNegCheck();
+	});
+
+});
+
+
