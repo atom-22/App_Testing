bfc62b51fe1233774d9d34d55f5cc21025700e71
Atom Ulikhanyan <atomulikhanyan@gmail.com>
2018-03-30 16:05:37 +0400

    changed path

--- loginPagePositiveTesting.js
+++ loginPagePositiveTesting.js
@@ -26,12 +26,13 @@ describe('Twitter: Email validation ', ()=> {
 			$(poTW.loginField.singUpEmail).clear().sendKeys(posVal[i]);
 			browser.sleep(1000);
 			expect(validEmail.isDisplayed()).toBe(true);
-			//expect( element(By.xpath('//*[@id="phx-signup-form"]/div[1]/div[2]/div/div[1]/p[1]')).isDisplayed()).toBe(true);
+			//expect(element(By.xpath('//*[@id="phx-signup-form"]/div[1]/div[2]/div/div[1]/p[1]')).isDisplayed()).toBe(true);
 			expect(emailError.isPresent()).toBeFalsy();
+			//expect(element(By.xpath('//*[@id="phx-signup-form"]/div[1]/div[3]/div[1]/div/p[4]')).isPresent()).toBeFalsy();
 		}	
 	}
 
-	it('should check positive values ', ()=> {
+	xit('should check positive values ', ()=> {
 		posCheck();
 	});
 
@@ -42,6 +43,7 @@ describe('Twitter: Email validation ', ()=> {
 			browser.sleep(1000);
 			expect(validEmail.isPresent()).toBeFalsy();
 			expect(emailError.getText()).toBe(test_data.emailErrorMessage);
+			//expect(element(By.xpath('//*[@id="phx-signup-form"]/div[1]/div[3]/div[1]/div/p[4]')).getText()).toBe(test_data.emailErrorMessage);
 		}
 	}
 
@@ -69,7 +71,7 @@ describe('Twitter: Password validation ', ()=> {
 		const posVal = test_data.passwordPosValues; 
 		for (var i = 0; i < posVal.length; ++i) {
 			$(poTW.loginField.passwordInput).clear().sendKeys(posVal[i]);
-			browser.sleep(5000);
+			browser.sleep(1000);
 			expect( element(By.xpath('//*[@id="phx-signup-form"]/div[1]/div[3]/div[1]/div/p[1]')).isDisplayed()).toBe(true);
 			expect($(poTW.loginField.error).isPresent()).toBeFalsy();
 		}	
@@ -83,7 +85,7 @@ describe('Twitter: Password validation ', ()=> {
 		const negVal = test_data.passwordNegValues;
 		for (var i = 0; i < negVal.length; ++i) {
 			$(poTW.loginField.passwordInput).clear().sendKeys(negVal[i]);
-			browser.sleep(5000);
+			browser.sleep(1000);
 			expect($(poTW.loginField.validPassword).isPresent()).toBeFalsy();
 			expect($(poTW.loginField.error).getText()).toBe(test_data.passordErrorMessage);
 		}
